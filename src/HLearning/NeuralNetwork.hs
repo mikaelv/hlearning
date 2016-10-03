3b6d9e1785bbf71c2862f08d174021bade114bb8
Mikael Valot <mikael.valot@gmail.com>
2016-10-03 20:54:23 +0100

    Gradient descent unit test ok

--- src/HLearning/NeuralNetwork.hs
+++ src/HLearning/NeuralNetwork.hs
@@ -11,8 +11,18 @@ import qualified Data.Vector.Storable as V
 import HLearning.Util
 
 
+trainNetwork :: [Int] -> [[Int]] -> (L 25 65, L 10 26)
+trainNetwork labels features =
+  let theta1Init = konst 1-- TODO random init
+      theta2Init = konst 1
+      Just y = (create . listToVector) labels :: Maybe (R 3823)
+      Just x = (create . LA.fromRows . map listToVector) features :: Maybe (L 3823 64)
+      initCost = cost x y theta1Init theta2Init
+  in (theta1Init, theta2Init)
 
--- @return label, feature vector
+listToVector = LA.vector . map fromIntegral
+
+-- returns label, feature vector
 parseLine :: String -> (Int, [Int])
 parseLine s =
   let ints = (reverse . read) ("[" ++ s ++ "]") :: [Int]
@@ -34,11 +44,32 @@ cost x y theta1 theta2 =
       a3 = sigmoid z3
       h = tr a3
       yb = toBinMatrix y
-      jmat = (-1 * yb) * log h
-      -- JMat = ((-1*YB) .* log(H))  -  ((1-YB) .* log(1 - H));
-      -- J = sum(sum(JMat)) / m;
-      j = 3
-  in j
+      jmat = yb * log h - yb * log (1 - h)
+      m = fromIntegral $ size y
+      -- TODO regularization
+  in - (LA.sumElements . extract) jmat / m
+
+
+backpropagate :: L 3823 10 -> Double -> L 25 65 -> L 10 26 -> L 25 3823 -> L 65 3823 -> L 26 3823 -> L 10 3823 -> Double
+backpropagate yb lambda theta1 theta2 z2 a1 a2 a3 =
+  let delta3 = a3 - tr yb
+      delta2a = tr theta2 <> delta3
+      delta2b = snd (splitRows delta2a :: (L 1 3823, L 25 3823)) -- TODO replace 3823 with m does not compile here !?
+      delta2 = delta2b * sigmoidGradient z2
+      d2 = delta3 <> tr a2
+      d1 = delta2 <> tr a1
+      m = 3823
+      regulCoef = lambda / m
+      gradTheta1 = (d1 / konst m) + (konst regulCoef * theta1)
+  in 3
+
+multScalar :: (KnownNat m, KnownNat n) => L m n -> Double -> L m n
+multScalar m d = m * konst d
+
+sigmoidGradient :: (KnownNat m, KnownNat n) => L m n -> L m n
+sigmoidGradient m = g * (1-g)
+  where g = sigmoid m
+
 
 --   -- read this: https://www.schoolofhaskell.com/user/konn/prove-your-haskell-for-great-safety/dependent-types-in-haskell
 sigmoid :: (KnownNat m, KnownNat n) => L m n -> L m n
--- test/Main.hs
+++ test/Main.hs
@@ -1,20 +1,36 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ImplicitParams, ScopedTypeVariables #-}
 
 module Main where
 
 import System.Exit (exitFailure)
 import Test.HUnit
+import Test.HUnit.Approx
 import Test.Framework.Providers.API
 import Test.Framework.Providers.HUnit
 import Test.Framework.Runners.Console (defaultMain)
 import Numeric.LinearAlgebra.Static
 import qualified Numeric.LinearAlgebra as LA
 import HLearning.Util
+import HLearning.GradientDescent
 
 testToBinaryVector = testCase "should transform the labels vector to a binary matrix" $ actual @?= expected
   where expected = extract (matrix [0,1,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,1] :: L 2 10)
         actual = extract . toBinMatrix $ vec2 1 9
 
-tests = testGroup "HLearning tests" [ testToBinaryVector ]
+
+testGradientDescent = testCase "should find the minimum of x^2 + 3*x" $ let ?epsilon = 0.0001 in actual @~? expected
+  where function x = x^2 + 3*x
+        expected = -3/2
+        tolerance = 1e-9
+        stopCond = stopCondition function tolerance
+        alpha = 0.1
+        initValue = 12
+        actual = unArg $ gradientDescent function (stopCondition function tolerance) alpha (Arg initValue)
+
+tests = testGroup "HLearning tests" [ testToBinaryVector, testGradientDescent ]
 
 main = defaultMain [tests]
+
+assertFloatEqual text a b =
+  assertEqual text (take 4 (show a)) (take 4 (show b))
