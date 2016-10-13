dcb50f5b3dea6edccf84f6e1fa68c4fe6a3bffcd
Mikael Valot <mikael.valot@gmail.com>
2016-10-13 21:49:59 +0100

    refactoring, ran some predictions but fails miserably

--- executables/Main.hs
+++ executables/Main.hs
@@ -8,18 +8,50 @@ import HLearning.NeuralNetwork
 import qualified Numeric.LinearAlgebra as LA
 import Numeric.LinearAlgebra.Static
 
+main = mainCoursera
 
-main = do
+mainNIST = do
   (labels, features) <- readTrainingData
   theta1 <- LA.rand 25 65
   theta2 <- LA.rand 10 26
   let Just theta1Typed = create theta1
       Just theta2Typed = create theta2
       initModel = Theta theta1Typed theta2Typed
-  print (trainNetwork labels features initModel)
+      trainedModel = trainNetwork labels features initModel
+
+  print "prediction for 1st example"
+  (tstLabels, tstFeatures) <- readTestData
+  print $ predict trainedModel (features !! 0)
+  print $ predict trainedModel (features !! 1)
+  print $ predict trainedModel (features !! 2)
+  print $ predict trainedModel (features !! 3)
+  print $ predict trainedModel (features !! 4)
+  print $ predict trainedModel (features !! 5)
+
+
+
+mainCoursera = do
+  Just theta1 <- 
+
 --  mapM putStrLn $ fmap show training
 
 --readTrainingData :: IO [(Integer, [Integer])]
-readTrainingData =
-  let file = readFile "/Users/mikael/Documents/boulotmik/projects/hlearning/data/optdigits.tra"
+readTrainingData = readData "/Users/mikael/Documents/boulotmik/projects/hlearning/data/optdigits.tra"
+readTestData     = readData "/Users/mikael/Documents/boulotmik/projects/hlearning/data/optdigits.tes"
+
+--readCourseraTheta1 =
+
+readCsvFile f =
+  let file = readFile f
+  in fmap parseCsvFile file
+
+  -- returns label, feature vector
+parseCsvLine :: String -> [Int]
+parseCsvLine s = read ("[" ++ s ++ "]") :: [Int]
+
+parseCsvFile :: String -> [[Int]]
+parseCsvFile s = parseCsvLine <$> lines s
+
+readData f =
+  let file = readFile f
   in fmap parseFile file
--- src/HLearning/NeuralNetwork.hs
+++ src/HLearning/NeuralNetwork.hs
@@ -11,24 +11,47 @@ import qualified Numeric.LinearAlgebra as LA
 import qualified Data.Vector.Storable as V
 import HLearning.Util
 import HLearning.GradientDescent
+import Debug.Trace
 
 data NNModel = Theta { theta1 :: L 25 65, theta2 :: L 10 26 } deriving Show
+
+predict :: NNModel -> [Int] -> Int
+predict nnModel features =
+  let Just x = listToVector features :: Maybe (R 64)
+      (h, a3, z3, a2, z2, a1) = feedForward nnModel (row x)
+  in (V.maxIndex . unwrap . unrow) h
+
+-- m: number of training examples
+feedForward :: (KnownNat m) => NNModel -> L m 64 -> (L m 10, L 10 m, L 10 m, L 26 m, L 25 m, L 65 m)
+feedForward (Theta theta1 theta2) x =
+  let a1 = (addOnes . tr) x
+      z2 = theta1 <> a1
+      a2 = addOnes $ sigmoid z2
+      z3 = theta2 <> a2
+      a3 = sigmoid z3
+      h = tr a3
+  in (h, a3, z3, a2, z2, a1)
 
 
 trainNetwork :: [Int] -> [[Int]] -> NNModel -> NNModel
 trainNetwork labels features initModel =
-  let Just y = (create . listToVector) labels :: Maybe (R 3823)
-      Just x = (create . LA.fromRows . map listToVector) features :: Maybe (L 3823 64)
+  let Just y = listToVector labels :: Maybe (R 3823)
+      Just x = listOfListToMatrix features :: Maybe (L 3823 64)
       lambda = 1 -- regularisation
       function = cost x y lambda
-      tolerance = 1e-9
-      stop (Params prev) (Params cur) = abs (fst (function prev) - fst (function cur)) < tolerance
+      tolerance = 1e-5
+      --stop (Params prev) (Params cur) = abs (fst (function prev) - fst (function cur)) < tolerance
+      stop (Params prev) (Params cur) = abs (fst (function cur)) < 0.1
       stopCond = StopWhen stop
-      alpha = 0.1
+      alpha = 0.01
       nnModel = model $ gradientDescent function stopCond alpha (Params initModel)
   in nnModel
 
-listToVector = LA.vector . map fromIntegral
+listToVector :: KnownNat m => [Int] -> Maybe (R m)
+listToVector lst = (create . LA.vector . map fromIntegral) lst :: KnownNat m1 => Maybe (R m1)
+
+listOfListToMatrix :: (KnownNat m, KnownNat n) => [[Int]] -> Maybe (L m n)
+listOfListToMatrix listOfList = (create . LA.fromRows . map (LA.vector . map fromIntegral) ) listOfList :: (KnownNat m1, KnownNat n1) => Maybe (L m1 n1)
 
 -- returns label, feature vector
 parseLine :: String -> (Int, [Int])
@@ -44,36 +67,31 @@ parseFile s = unzip $ parseLine <$> lines s
 -- theta1 : nb of rows in hidden layer * (nb_of_features +1)
 -- theta2 : nb of rows in output layer * (nb_of_rows(theta1) +1)
 cost :: L 3823 64 -> R 3823 -> Double -> NNModel -> (Double, NNModel)
-cost x y lambda (Theta theta1 theta2) =
-  let a1 = (addOnes . tr) x
-      z2 = theta1 <> a1
-      a2 = addOnes $ sigmoid z2
-      z3 = theta2 <> a2
-      a3 = sigmoid z3
-      h = tr a3
+cost x y lambda nnModel =
+  let (h, a3, z3, a2, z2, a1) = feedForward nnModel x
       yb = toBinMatrix y
       jmat = yb * log h - yb * log (1 - h)
       m = fromIntegral $ size y
       -- TODO regularization
       j = - (LA.sumElements . extract) jmat / m
-      (gradTheta1, gradTheta2) = backpropagate yb lambda theta1 theta2 z2 a1 a2 a3
-  in (j, Theta gradTheta1 gradTheta2)
+      (gradTheta1, gradTheta2) = backpropagate yb lambda nnModel z2 a1 a2 a3
+  in (traceShowId j, Theta gradTheta1 gradTheta2)
 
 
-backpropagate :: L 3823 10 -> Double -> L 25 65 -> L 10 26 -> L 25 3823 -> L 65 3823 -> L 26 3823 -> L 10 3823 -> (L 25 65, L 10 26)
-backpropagate yb lambda theta1 theta2 z2 a1 a2 a3 =
+backpropagate :: L 3823 10 -> Double -> NNModel -> L 25 3823 -> L 65 3823 -> L 26 3823 -> L 10 3823 -> (L 25 65, L 10 26)
+backpropagate yb lambda (Theta theta1 theta2) z2 a1 a2 a3 =
   let delta3 = a3 - tr yb
       delta2a = tr theta2 <> delta3
       delta2b = snd (splitRows delta2a :: (L 1 3823, L 25 3823)) -- TODO replace 3823 with m does not compile here !?
       delta2 = delta2b * sigmoidGradient z2
       d2 = delta3 <> tr a2
       d1 = delta2 <> tr a1
-      m = 3823
+      m = (fromIntegral . fst . size) yb
       gradTheta1NonReg  = (d1 / konst m)
       gradTheta2NonReg  = (d2 / konst m)
-      regulCoef = lambda / m
-      gradTheta1 = (d1 / konst m) + (konst regulCoef * theta1) -- TODO not for the first column
-  in (gradTheta1NonReg, gradTheta2NonReg)
+--      regulCoef = lambda / m
+--      gradTheta1 = (d1 / konst m) + (konst regulCoef * theta1) -- TODO not for the first column
+  in (gradTheta1NonReg, gradTheta2NonReg) -- TODO regularization
 
 
 
@@ -84,6 +102,15 @@ instance GradientDescent (NNModel -> (Double, NNModel)) where
   grad f (Params nnModel) =
     let (cost, gradTheta) = f nnModel
     in Params gradTheta
+
+  -- Use numeric differentiation for taking the gradient.
+  -- TODO compute for each element of the unrolled vector
+  -- grad f (Params nnModel) =
+  --   let (costA, _) = f nnModel
+  --       (costB, _) = f (nnModel - konst epsilon)
+  --   in Params $ (costA - costB) / epsilon
+  --   where epsilon = 0.0001
+
 
 
   paramMove scale (Params (Theta gradTheta1 gradTheta2)) (Params (Theta theta1 theta2)) =
